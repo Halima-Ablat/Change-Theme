0ddb9df13f435c25e344901f0e50ed98fd9870d0
Halima <152661929+Halima-Ablat@users.noreply.github.com>
2025-03-28 22:39:47 +1100

    Update ChangeTheme.jsx

--- ChangeTheme.jsx
+++ ChangeTheme.jsx
@@ -1,33 +1,22 @@
-import React, {  useState, useRef } from "react";
+import LocalStorage from "./LocalStorage";
 
-function ChangeTheme() {
-   const [styles, setStyles] = useState({
-    color: "black",
-    backgroundColor: "white",
-    buttonColor: "white",
-    buttonBackgroundColor: "black",
-    borderColor: "white"
-   })
+function ColorChange() {
+  const [color, setColor] = LocalStorage('color', 'black')
 
-  function changeColor() {
-    setStyles(prevStyles => ({ color: prevStyles.color === "white" ? "black" : "white", backgroundColor: prevStyles.backgroundColor === "white" ? "black" : "white", buttonColor: prevStyles.buttonColor === "black" ? "white" : "black", buttonBackgroundColor: prevStyles.buttonBackgroundColor === "white" ? "black" : "white", 
-      borderColor: prevStyles.borderColor === "white" ? "black" : "white" })); 
-      document.body.style.backgroundColor = styles.backgroundColor === "white" ? "black" : "white"; }
+  function handleColorChange() {
+    setColor(color === "white" ? "black" : "white");
+  }
+
+  console.log(color);
 
   return (
-    <>
+    <div className="color-mode" data-color={color}>
       <div className="container">
-        <h1 style={{color: styles.color}}>Hello World !</h1>
-        <button onClick={changeColor} style={{
-          color: styles.buttonColor,
-          backgroundColor: styles.buttonBackgroundColor,
-          borderColor: styles.borderColor
-        }} >
-          Change Theme
-        </button>
+        <h1>Hello World !</h1>
+        <button onClick={handleColorChange}>Change Theme</button>
       </div>
-    </>
+    </div>
   );
 }
 
-export default ChangeTheme;
+export default ColorChange;
